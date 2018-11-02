fd5c5deeba7d2c066511c2090d86daef7444c66b
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-11-02 11:53:30 -0700

    basic nodejs setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req,res)=>{
+    res.send('Hello!');
+})
+
+app.listen(5000, ()=>{
+    console.log('App listening to port 5000');
+})
