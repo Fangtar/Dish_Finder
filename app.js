872dd66e320af0a9eb3df47af663e0d8f3fd2b56
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-02 12:14:41 -0700

    added bootstrap

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.get('/', (req,res)=>{
-    res.send('Hello!');
+    res.render("home.handlebars")
 })
 
 app.listen(5000, ()=>{
