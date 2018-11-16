771500da0b8edd2ac2b710bd0aea52c3f4684207
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-11-15 16:54:36 -0800

    implemented search function

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,7 +1,30 @@
+
+const yelp = require('yelp-fusion');
+const apiKey = 'Ttm9T3jW7ItGLfoEF1IaUztpCbCc5BVHFgzNjAbKgz2iyEP9JTBsjYJFxYz-HBux6r4GGROqZb8AWno1CvxmretUb7l7L79FUOByZGk9s2SX9kWEXF2X3BNPcuPlW3Yx';
+// const searchRequest = {
+//     term:'Burrito',
+//     location: '2311 Warring Street, Berkeley, CA'
+// };
+const client = yelp.client(apiKey);
+
+
 module.exports = app => {
-    
+
     app.get("/stores", function(req, res) {
-        res.render("show-results")
+        let query_input = req.query;
+        console.log(query_input);
+        // query_input.location = '2311 Warring Street, Berkeley, CA';
+        // console.log(query_input);
+        client.search(query_input).then(response => {
+            let results = response.jsonBody.businesses;
+            // console.log(results);
+            // const firstResult = response.jsonBody.businesses[2];
+            // const prettyJson = JSON.stringify(firstResult, null, 4);
+            // console.log(prettyJson);
+            res.render("show-results", {results: results})
+        }).catch((err) => {
+            console.log(err.message);
+        });
     })
 
 };
