bbfb26256254303efd4589bce36f45c24fa97b3d
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-15 16:09:12 -0800

    added the code to server.js

--- yelp-fusion-api/server.js
+++ yelp-fusion-api/server.js
@@ -10,3 +10,30 @@ client.search({
 }).catch(e => {
     console.log(e);
 });
+
+
+
+// New yelp api code
+
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+// Place holder for Yelp Fusion's API Key. Grab them
+// from https://www.yelp.com/developers/v3/manage_app
+const apiKey = '';
+
+const searchRequest = {
+  term:'Burrito',
+  location: '2311 Warring Street, Berkeley, CA'
+};
+
+const client = yelp.client(apiKey);
+
+client.search(searchRequest).then(response => {
+  const firstResult = response.jsonBody.businesses[2];
+  const prettyJson = JSON.stringify(firstResult, null, 4);
+  console.log(prettyJson);
+}).catch(e => {
+  console.log(e);
+});
